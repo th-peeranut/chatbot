11171bf68b0c3a06a8db0e3b8c031acc7d238dba
Th-Peeranut <th.peeranut@gamil.com>
2022-08-06 22:03:21 +0700

    add google dialogflow

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const axios = require('axios').default;
 const dotenv = require('dotenv');
 
 const env = dotenv.config().parsed;
 const app = express();
+const port = 3000
 
 const lineConfig = {
     channelAccessToken: env.ACCESS_TOKEN,
@@ -14,15 +17,56 @@ const lineConfig = {
 // create client
 const client = new line.Client(lineConfig);
 
-app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
-    try {
-        const events = req.body.events
-        return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK");
+let roundRobin = 0;
+const agentHook_urls = [
+    'https://dialogflow.cloud.google.com/v1/integrations/line/webhook/9631574a-6d16-4c03-afcf-26053eab6313'
+];
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/webhook', async (req, res) => {
+    const body = req.body;
+    if (++roundRobin >= agentHook_urls.length) roundRobin = 0;
+
+    const agent_url = agentHook_urls[roundRobin];
+
+    // await fetch(agent_url, {
+    //     method: 'post',
+    //     body: JSON.stringify(body),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    // .then(res => res.json())
+    // .then(json => {
+    //     console.log(json);
+    //     res.send(json);
+    // })
+    await axios.post(agent_url, body, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then()
+    .catch(err => {
+        console.log(err);
+    })
     
-    } catch (error) {
-        res.status(500).end();
-    }
 });
+
+// app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
+//     try {
+//         const events = req.body.events
+//         return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK");
+    
+//     } catch (error) {
+//         res.status(500).end();
+//     }
+// });
 
 const handleEvent = async (event) => {
     if (event.type !== 'message' || event.message.type !== 'text') {
@@ -35,6 +79,6 @@ const handleEvent = async (event) => {
     }
 };
 
-app.listen(4000, () => {
-    console.log('listening on 4000');
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
